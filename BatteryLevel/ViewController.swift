0b61ac65fd955cff9913f333dd743cab165eec0d
shoon0224 <dltkdgns204@naver.com>
2021-03-07 14:07:32 +0900

    배터리 상태를 나타내는 ProgressView 추가 및 배터리 값에 따라 ProgressView Tint Color 변경

--- BatteryLevel/ViewController.swift
+++ BatteryLevel/ViewController.swift
@@ -10,30 +10,35 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var lblMyBattery: UILabel!
+    @IBOutlet var pvBatteryView: UIProgressView!
+    
+
     var myBattery: Float = 0.0 //내 배터리 변수 초기화
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        
         let batteryLevel = UIDevice.current.batteryLevel*100
         
         print("My Battery:\(batteryLevel)")
         
-        myBattery = batteryLevel
         
+        myBattery = batteryLevel
+        pvBatteryView.progress = myBattery/100 //프로그레스 배터리값
         
         
         lblMyBattery.text = String(batteryLevel)
         switch myBattery {
         case 80.0...100.0:
-            self.view.backgroundColor = UIColor.green
+            pvBatteryView.progressTintColor = UIColor.green
         case 50.0..<80.0:
-            self.view.backgroundColor = UIColor.blue
+            pvBatteryView.progressTintColor = UIColor.blue
         case 20.0..<50.0:
-            self.view.backgroundColor = UIColor.brown
+            pvBatteryView.progressTintColor = UIColor.brown
         default:
-            self.view.backgroundColor = UIColor.red
+            pvBatteryView.progressTintColor = UIColor.red
         }
     }
 
